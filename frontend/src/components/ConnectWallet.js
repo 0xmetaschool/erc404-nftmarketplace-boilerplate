1d51ab870a25ae5906ca31bcd3d8a77211e65908
MunimIftikhar <129931419+munimmetaschool@users.noreply.github.com>
2024-02-20 18:19:27 +0500

    Update ABIs and Addresses

--- frontend/src/components/ConnectWallet.js
+++ frontend/src/components/ConnectWallet.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState, createContext, useContext } from 'react';
-import Web3 from 'web3';
-import NFTMintDN404 from '../contracts/NFTMintDN404.json';
-import NFTMarketplaceABI from '../contracts/NFTMarketplace.json';
+import React, { useEffect, useState, createContext, useContext } from "react";
+import Web3 from "web3";
+import NFTMintDN404 from "../contracts/NFTMintDN404.json";
+import NFTMarketplaceABI from "../contracts/NFTMarketplace.json";
 
 // Create a context for Web3
 const Web3Context = createContext();
@@ -16,8 +16,8 @@ function ConnectWallet({ children }) {
   const [account, setAccount] = useState(null);
   const [marketplaceContract, setMarketplaceContract] = useState(null);
 
-  const marketplaceAddress = "0x0Abef8EDAC7A1be16F8A5595f71c14dc395A0773";
-  const nftContractAddress = "0xd8E46D75B5f4b450534acA1804f1CfcbeDEA3772";
+  const marketplaceAddress = "YOUR_MARKETPLACE_ADDRESS";
+  const nftContractAddress = "YOUR_NFT_CONTRACT_ADDRESS";
 
   useEffect(() => {
     if (window.ethereum) {
@@ -32,12 +32,18 @@ function ConnectWallet({ children }) {
         const accounts = await web3.eth.getAccounts();
         const account = accounts[0];
         console.log(account);
-        const marketplaceContract = new web3.eth.Contract(NFTMarketplaceABI.abi, marketplaceAddress);
-        const tokenContract = new web3.eth.Contract(NFTMintDN404.abi, nftContractAddress);
+        const marketplaceContract = new web3.eth.Contract(
+          NFTMarketplaceABI.abi,
+          marketplaceAddress
+        );
+        const tokenContract = new web3.eth.Contract(
+          NFTMintDN404.abi,
+          nftContractAddress
+        );
         setMarketplaceContract(marketplaceContract);
         setNftContract(tokenContract);
         setAccount(account);
-        // Set default account 
+        // Set default account
         marketplaceContract.options.from = account;
         tokenContract.options.from = account;
       } catch (error) {
@@ -48,15 +54,25 @@ function ConnectWallet({ children }) {
 
   const disconnectWallet = () => {
     setAccount(null);
-    setMarketplaceContract(null); 
+    setMarketplaceContract(null);
     setNftContract(null);
   };
 
   return (
-    <Web3Context.Provider value={{ web3, signer, nftContract, account, marketplaceContract, marketplaceAddress, nftContractAddress, disconnectWallet, connectWallet }}>
-      <div>
-        {children}
-      </div>
+    <Web3Context.Provider
+      value={{
+        web3,
+        signer,
+        nftContract,
+        account,
+        marketplaceContract,
+        marketplaceAddress,
+        nftContractAddress,
+        disconnectWallet,
+        connectWallet,
+      }}
+    >
+      <div>{children}</div>
     </Web3Context.Provider>
   );
 }
